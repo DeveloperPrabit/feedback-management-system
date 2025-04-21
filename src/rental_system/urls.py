14ac538887ced47a9115d927a7495dcd3dde3495
acharyapuru <acharyapuru@users.noreply.github.com>
2025-04-21 12:03:41 +0545

    serve media and static files

--- src/rental_system/urls.py
+++ src/rental_system/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 admin.site.site_header = 'Rental System Admin'
 admin.site.site_title = 'Rental System Admin'
@@ -28,7 +29,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
     path('', include('tenants.urls')),
-    path('', include('invoices.urls'))
+    path('', include('invoices.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
